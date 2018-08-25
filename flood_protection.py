7ef0697432cc5d0f8c6ef8bb9fd28d47799d5c11
Abhinav Gautam <abhinav.nanhey@gmail.com>
2018-08-25 19:26:12 +0530

    fixed duplication

--- flood_protection.py
+++ flood_protection.py
@@ -29,9 +29,9 @@ class Spam_settings:
                         if amount > self.limits[limit]:
                             self.timeouts[chat_id] = update_time + self.timeout_start * (self.factors[chat_id])
                             self.factors[chat_id] *= self.timeout_factor
-                            text = "You are timeouted by the flood protection system of this bot. Try again in {0} seconds.".format(
-                                self.timeouts[chat_id] - update_time)
-
+                            text = "You are timeouted by the flood protection" \
+                                   " system of this bot. Try again in {0} seconds."\
+                                .format(self.timeouts[chat_id] - update_time)
                             return text
         self.times[chat_id].insert(0, update_time)
         return 0
@@ -40,32 +40,27 @@ class Spam_settings:
         def func_wrapper(bot, update, *args2):
             timeout = self.new_message(update.effective_chat.id)
             if not timeout:
-               return func(bot, update, *args2)
+                return func(bot, update, *args2)
             elif isinstance(timeout, str):
-                print("timeout")
-                # Only works for messages (+Commands) and callback_queries (Inline Buttons)
-                if update.callback_query:
-                    bot.edit_message_text(chat_id=update.effective_chat.id,
-                                          message_id=update.effective_message.message_id,
-                                          text=timeout)
-                elif update.message:
-                    bot.send_message(chat_id=update.effective_chat.id, text=timeout)
-
+                self.message_sender(bot, update, timeout)
         return func_wrapper
 
     def wrapper_for_class_methods(self, func):  # only works on functions, not on instancemethods
         def func_wrapper(class_obj, bot, update, *args2):
             timeout = self.new_message(update.effective_chat.id)
             if not timeout:
-               return func(class_obj, bot, update, *args2)
+                return func(class_obj, bot, update, *args2)
             elif isinstance(timeout, str):
-                print("timeout")
-                # Only works for messages (+Commands) and callback_queries (Inline Buttons)
-                if update.callback_query:
-                    bot.edit_message_text(chat_id=update.effective_chat.id,
-                                          message_id=update.effective_message.message_id,
-                                          text=timeout)
-                elif update.message:
-                    bot.send_message(chat_id=update.effective_chat.id, text=timeout)
-
+                self.message_sender(bot, update, timeout)
         return func_wrapper
+
+    @staticmethod
+    def message_sender(bot, update, timeout):
+        print("timeout")
+        # Only works for messages (+Commands) and callback_queries (Inline Buttons)
+        if update.callback_query:
+            bot.edit_message_text(chat_id=update.effective_chat.id,
+                                  message_id=update.effective_message.message_id,
+                                  text=timeout)
+        elif update.message:
+            bot.send_message(chat_id=update.effective_chat.id, text=timeout)
